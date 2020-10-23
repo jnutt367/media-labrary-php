3068f1727a42259ca0482d2f336edf59c1461892
Jason Nutt <jnutt367@gmail.com>
2020-10-23 15:10:35 -0400

    commit details and changes to other files

--- details.php
+++ details.php
@@ -9,7 +9,7 @@ include("includes/functions.php");
 
 if (isset($_GET["id"])){
 	$id = $_GET["id"];
-	if (isset(catalog[$id])) {
+	if (isset($catalog[$id])) {
 		$item = $catalog[$id];
 	}
 }
@@ -30,6 +30,63 @@ include("includes/header.php"); ?>
 			<span>
 			<img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
 			</span>
+			</div>
+			<div class="media-details">
+				<h1><?php echo $item["title"]; ?></h1>
+				<!--- This is the start of the item's details in a table format----->
+				<table>
+				
+					<tr>
+						<th>Category</th>
+						<td><?php echo $item["category"]; ?></td>
+					</tr>
+					<tr>
+						<th>Genre</th>
+						<td><?php echo $item["genre"]; ?></td>
+					</tr>
+					<tr>
+						<th>Format</th>
+						<td><?php echo $item["format"]; ?></td>
+					</tr>
+					<tr>
+						<th>Year</th>
+						<td><?php echo $item["year"]; ?></td>
+					</tr>
+					<?php if (strtolower($item["category"]) == "books") { ?>
+						<tr>
+							<th>Authors</th>
+							<td><?php echo implode(", ", $item["authors"]); ?></td>
+						</tr>
+						<tr>
+							<th>Publisher</th>
+							<td><?php echo $item["publisher"]; ?></td>
+						</tr>
+						<tr>
+							<th>ISBN</th>
+							<td><?php echo $item["isbn"]; ?></td>
+						</tr>
+					<?php } else if (strtolower($item["category"]) == "movies") { ?>
+						<tr>
+							<th>Director</th>
+							<td><?php echo $item["director"]; ?></td>
+						</tr>
+						<tr>
+							<th>Writers</th>
+							<td><?php echo implode(", ", $item["writers"]); ?></td>
+						</tr>
+						<tr>
+							<th>Stars</th>
+							<td><?php echo implode(", ", $item["stars"]); ?></td>
+						</tr>
+						<?php } else if (strtolower($item["category"]) == "music") { ?>
+						<tr>
+							<th>Artist</th>
+							<td><?php echo $item["artist"]; ?></td>
+						</tr>
+					<?php } ?>
+				</table>
+			
+			</div>
 		</div>
 	</div>
 </div>
